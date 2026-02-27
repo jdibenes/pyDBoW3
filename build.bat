37e94656ce1d26940ad4b09904557d3f5150fd63
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-02-26 19:02:22 -0500

    build windows boost 1.90.0 opencv 3.4.16 and vs 2019

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ cd ..
 mkdir build
 cd build
 
-cmake -DBUILD_PYTHON3=ON -DBUILD_STATICALLY_LINKED=OFF -DPYTHON_EXECUTABLE=C:/Users/jcds/AppData/Local/Programs/Python/Python36/python.exe -DPYTHON_INCLUDE_DIR=C:/Users/jcds/AppData/Local/Programs/Python/Python36/include -DPYTHON_LIBRARY=C:/Users/jcds/AppData/Local/Programs/Python/Python36/libs/python36.lib -DBOOST_ROOT=C:/Users/jcds/SDK/boost_1_87_0 -DOpenCV_DIR=C:/Users/jcds/SDK/opencv_3414/build -DDBoW3_DIR=../install/DBow3/build -DDBoW3_INCLUDE_DIRS=../install/DBow3/src -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" ../src
+cmake -DBUILD_PYTHON3=ON -DBUILD_STATICALLY_LINKED=OFF -DPYTHON_EXECUTABLE=C:/Users/jdibe/AppData/Local/Programs/Python/Python36/python.exe -DPYTHON_INCLUDE_DIR=C:/Users/jdibe/AppData/Local/Programs/Python/Python36/include -DPYTHON_LIBRARY=C:/Users/jdibe/AppData/Local/Programs/Python/Python36/libs/python36.lib -DBOOST_ROOT=D:/jcds/SDK2/boost_1_90_0 -DOpenCV_DIR=D:/jcds/SDK2/opencv_3_4_16/build -DDBoW3_DIR=../install/DBow3/build -DDBoW3_INCLUDE_DIRS=../install/DBow3/src -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" ../src
 
 cd ..
--- install/dbow3.bat
+++ install/dbow3.bat
@@ -3,7 +3,7 @@ cd DBow3
 mkdir build
 cd build
 
-cmake -DLIB_INSTALL_DIR=lib -DOpenCV_DIR=C:/Users/jcds/SDK/opencv_3414/build -DBUILD_SHARED_LIBS=OFF -DUSE_CONTRIB=ON -DBUILD_UTILS=OFF -G "Visual Studio 16 2019" .. 
+cmake -DLIB_INSTALL_DIR=lib -DOpenCV_DIR=D:/jcds/SDK2/opencv_3_4_16/build -DBUILD_SHARED_LIBS=OFF -DUSE_CONTRIB=ON -DBUILD_UTILS=OFF -G "Visual Studio 16 2019" .. 
 
 cd ..
 cd ..
